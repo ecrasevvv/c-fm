a92b58a9ddf932aa969652d5b39f14fb0c8994f3
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-04 18:52:10 +0200

    cfm_get_tensor_strides to calculate strides given t->shape and minor changes

--- cfm.c
+++ cfm.c
@@ -54,6 +54,19 @@ static size_t cfm_element_size(cfm_dtype dtype) {
     return (dtype == CFM_FLOAT32) ? sizeof(float) : sizeof(double);
 }
 
+/* This function returns the stride (https://numpy.org/doc/2.1/reference/arrays.ndarray.html#arrays-ndarray
+ * in the "Internal memory layout of an ndarray) which corresponds to the bytes to step in each dimension
+ * when traversing an array. */
+static void cfm_set_tensor_strides(cfm_tensor *t) {
+    /* strides[k] = shape[k+1] * shape[k+2] * ... * shape[ndims] */
+    for (size_t k = 0; k < t->ndims; ++k) {
+        t->strides[k] = 1;
+        for (size_t i = k+1; i < t->ndims; ++i) {
+            t->strides[k] *= t->shape[i];
+        }
+    }
+}
+
 cfm_tensor *cfm_tensor_new(cfm_string name, cfm_dtype dtype,
         uint8_t ndims, uint16_t *shape, bool requires_grad) {
     cfm_tensor *t = (cfm_tensor*)malloc(sizeof(cfm_tensor));
@@ -64,6 +77,7 @@ cfm_tensor *cfm_tensor_new(cfm_string name, cfm_dtype dtype,
     memcpy(t->shape, shape, sizeof(uint16_t) * ndims);
     t->numel = 1;
     for (size_t i = 0; i < t->ndims; ++i) t->numel *= t->shape[i]; 
+    cfm_set_tensor_strides(t);
     if (!(t->data = calloc(t->numel, cfm_element_size(t->dtype))))  /* zero-init new cfm_tensor data. */
         cfm_die("Out of memory");
     t->requires_grad = requires_grad;
@@ -79,9 +93,10 @@ cfm_tensor *cfm_tensor_from(cfm_string name, cfm_dtype dtype,
     t->ndims = ndims;
     memcpy(t->shape, shape, sizeof(uint16_t) * ndims);
     t->data = data;
-    // ??? ERROR
-    //size_t element_size = cfm_element_size(dtype);
-    //t->numel = (sizeof(data)/element_size);
+    t->numel = 1;
+    for (size_t i = 0; i < t->ndims; ++i) t->numel *= t->shape[i]; 
+    cfm_set_tensor_strides(t);
+    size_t element_size = cfm_element_size(dtype);
     t->requires_grad = requires_grad;
     return t;
 }
@@ -116,14 +131,14 @@ cfm_tensor *cfm_tensor_randn(cfm_string name, cfm_dtype dtype,
         for (size_t i = 0; i < t->numel; ++i) {
             float u1 = (float)rand()/(float)RAND_MAX;
             float u2 = (float)rand()/(float)RAND_MAX;
-            d[i] = sqrtf(-2.f*log((float)u1)) * cosf(_2_PI*u2);
+            d[i] = sqrtf(-2.f*log((float)u1)) * cosf(CFM_2_PI*u2);
         }
     } else {
         double *d = t->data;
         for (size_t i = 0; i < t->numel; ++i) {
             double u1 = (double)rand()/(double)RAND_MAX;
             double u2 = (double)rand()/(double)RAND_MAX;
-            d[i] = sqrt(-2.f*log(u1)) * cos(_2_PI*u2);
+            d[i] = sqrt(-2.f*log(u1)) * cos(CFM_2_PI*u2);
         }
     }
     return t;
@@ -134,36 +149,36 @@ static double cfm_tensor_get_element(const cfm_tensor *t, uint64_t idx) {
     return (t->dtype == CFM_FLOAT32) ? ((float*)t->data)[idx] : ((double*)t->data)[idx];
 }
 
-void cfm_tensor_print_raw(const cfm_tensor *t, cfm_print_mode pm) {
+void cfm_tensor_print_raw(const cfm_tensor *t, cfm_print_mode pm, int precision) {
     printf("%s(", t->name.content);
     switch (pm) {
         case CFM_H_PRINT:
             for (size_t i = 0; i < t->numel; ++i) {
-                if (i == t->numel-1) printf("%f)\n", cfm_tensor_get_element(t, i));
-                else printf("%f, ", cfm_tensor_get_element(t, i));
+                if (i == t->numel-1) printf("%.*f)\n", precision, cfm_tensor_get_element(t, i));
+                else printf("%.*f, ", precision, cfm_tensor_get_element(t, i));
             }
             break;
         case CFM_V_PRINT:
             printf("\n");
             size_t indent = strlen(t->name.content);
             for (size_t i = 0; i < t->numel; ++i) {
-                if (i == t->numel-1) printf("%*s%f)\n", (int)indent, "", cfm_tensor_get_element(t, i));
-                else printf("%*s%f,\n", (int)indent, "", cfm_tensor_get_element(t, i));
+                if (i == t->numel-1) printf("%*s%.*f)\n", (int)indent, "", precision, cfm_tensor_get_element(t, i));
+                else printf("%*s%.*f,\n", (int)indent, "", precision, cfm_tensor_get_element(t, i));
             }
             break;
     }
 }
 
-/* This function returns the stride (https://numpy.org/doc/2.1/reference/arrays.ndarray.html#arrays-ndarray
- * in the "Internal memory layout of an ndarray) which corresponds to the bytes to step in each dimension
- * when traversing an array. */
-//static cfm_get_strides...
-
-void cfm_tensor_print(const cfm_tensor *t) {
+void cfm_tensor_print(const cfm_tensor *t, int precision) {
+    CFM_ASSERT(precision > 0 && precision <= 6);
     // (1, 2, 3, 4, 5, 6)       IN MEMORY, stored row major
     // [[1, 2, 3], [4, 5, 6]]   IN THIS cfm_tensor IMPLEMENTATION 
     /* for a shape[2, 3] tensor:
      *
      */
-    
+        
+    for (uint64_t i = 0; i < t->numel; ++i) {
+
+        printf("%.*f", precision, cfm_tensor_get_element(t, i));
+    }
 }
