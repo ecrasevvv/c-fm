8a34019bd743e14156b270781e935c5ac6f0a4ec
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-10 12:46:51 +0200

    removed useless files

--- cfm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "d" ]]; then
-    gcc "$2" cfm.c -DDEBUG -lm -Wall -Wextra -fsanitize=address -O2 -o cfm && ./cfm
-else
-    gcc test.c cfm.c -lm -Wall -Wextra -fsanitize=address -O2 -o cfm && ./cfm
-fi
