8cba2f688106509d5d4b8a7d89e55b04e8ffab24
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-20 13:07:42 +0200

    ~100 GFLOPS vs the ~125 numpy max. Still need to try other MR and NR kernel sizes

--- mm/mm.c
+++ mm/mm.c
@@ -1,10 +1,9 @@
 /*
- * M=N=512
- * K=256
+ * M=512,N=504,K=256
  *
  * Theoretical maximum on single core:  ~147 GFLOPS
- * numpy archives (single-core):        ~100 GFLOPS
- * Current best:                        nd
+ * numpy archives (single-core):        ~125 GFLOPS (10 NITER)
+ * Current best:                        ~100 GFLOPS (10 NITER) 
  */
 
 #include <stdio.h>
@@ -28,15 +27,10 @@
 #define ARR_TYPE float
 
 #define M 512
-#define N 512
+#define N 504
 #define K 256 
-#define BS 8
 #define MAX_VAL 10.f
 #define NITER 10
-
-#ifdef __AVX2__
-_Static_assert(BS%8==0, "BS must be a multiple of 8 when using AVX2 intrinsics and ARR_TYPE=float");
-#endif
 
 /* IDX formula = i*rows + j where:
  *      - i = col index of the related matrix
@@ -86,13 +80,85 @@ void baseline(const ARR_TYPE *A, const ARR_TYPE *B, ARR_TYPE *__restrict__ C) {
     }
 }
 
-void mm(const ARR_TYPE *A, const ARR_TYPE *B, ARR_TYPE *__restrict__ C) {
+/* AVX2 CPU contains 16 YMM registers.
+ * Each YMM register can store up to 8 floats (256 bits in total).
+ *
+ * Compute an m_{r} x n_{r} sub-matrix \tilde{C} of C. Where r denotes "register".
+ *
+ * Considering:
+ *  - \tilde{C} of size m_{r}   x n_{r} is equals to:
+ *  - \tilde{A} of size m_{r}   x K
+ *  - \tilde{B} of size K       x r_{r}
+ *
+ * \tilde{C} = \tilde{A}\tilde{B}
+ *
+ * Zero-init the \tilde{C} accumulator, load 1 column of \tilde{A} and one row
+ * of \tilde{B}. After K iterations the computation of \tilde{C} is completed
+ * computing the outer product of the two loaded vectors.
+ *
+ * In this case "load" means load from RAM to YMM registers. Each column of the 
+ * accumulator \tilde{C} can spans over more than 1 YMM register, the same goes
+ * for the \tilde{A} column vector.
+ *
+ * m_{r} must be divisible by 8.
+ *
+ * (m_{r}/8)*n_{r}  = total YMM registers needed to store the accumulator \tilde{C}
+ * m_{r}/8          = total YMM registers needed to store the \tilde{A} col-vector
+ * 1                = total YMM registers needed to store the \tilde{B} row-vector single broadcasted value
+ *
+ * (m_{r}/8 * n_{r} + m_{r}/8 + 1) <= 16
+ *
+ * for example: m_{r}=8 and n_{r}=12
+ */
+#define MR 16
+#define NR 6
+__attribute__((noinline))
+void kernel_16x6(float *A_start, float *B_start, float *__restrict__ C_start) {
+    __m256 acc[6][2] = {};
+    __m256 b_broadcast;
+    __m256 a0;
+    __m256 a1;
+
+    for (size_t p = 0; p < K; ++p) {
+        a0 = _mm256_loadu_ps(&A_start[p * M    ]);
+        a1 = _mm256_loadu_ps(&A_start[idx(p,M,8)]);
+    
+        b_broadcast = _mm256_broadcast_ss(&B_start[p]);
+        acc[0][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[0][0]);
+        acc[0][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[0][1]);
+
+        b_broadcast = _mm256_broadcast_ss(&B_start[idx(1,K,p)]);
+        acc[1][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[1][0]);
+        acc[1][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[1][1]);
+
+        b_broadcast = _mm256_broadcast_ss(&B_start[idx(2,K,p)]);
+        acc[2][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[2][0]);
+        acc[2][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[2][1]);
+
+        b_broadcast = _mm256_broadcast_ss(&B_start[idx(3,K,p)]);
+        acc[3][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[3][0]);
+        acc[3][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[3][1]);
+
+        b_broadcast = _mm256_broadcast_ss(&B_start[idx(4,K,p)]);
+        acc[4][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[4][0]);
+        acc[4][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[4][1]);
+
+        b_broadcast = _mm256_broadcast_ss(&B_start[idx(5,K,p)]);
+        acc[5][0] = _mm256_fmadd_ps(a0, b_broadcast, acc[5][0]);
+        acc[5][1] = _mm256_fmadd_ps(a1, b_broadcast, acc[5][1]);
+    }
+
+    for (size_t j = 0; j < 6; ++j) {
+        _mm256_storeu_ps(&C_start[j * M], acc[j][0]);
+        _mm256_storeu_ps(&C_start[idx(j,M,8)], acc[j][1]);
+    }
+}
+
+void mm(ARR_TYPE *A, ARR_TYPE *B, ARR_TYPE *__restrict__ C) {
     // A[M][K], B[K][N], C[M][N]
-    for (size_t i = 0; i < M; ++i) {
-        for (size_t j = 0; j < N; ++j) {
-            for (size_t p = 0; p < K; ++p) {
-                C[idx(j,M,i)] += A[idx(p,M,i)] * B[idx(j,K,p)];
-            }
+    for (size_t i = 0; i < M; i+=MR) {
+        for (size_t j = 0; j < N; j+=NR) {
+            kernel_16x6(&A[i], &B[j*K], &C[idx(j,M,i)]);
         }
     }
 }
@@ -124,7 +190,7 @@ void check(ARR_TYPE *__restrict__ C, ARR_TYPE *__restrict__ _C) {
 }
 
 void summary(void) {
-#if defined(__AVX2__)
+#ifdef __AVX2__
     printf("Running AVX2 instructions.\n");
 #endif
 #if defined(__linux__)
@@ -143,9 +209,7 @@ void summary(void) {
 #if defined(__linux__)
     printf("Elements per L1 cache line: %d\n", (int)l1_cache/(int)sizeof(ARR_TYPE));
 #endif
-    printf("Block dimension:            %dx%d\n", BS, BS);
-    printf("Elements per block:         %d\n", BS*BS);
-    printf("Block size (bytes):         %ld\n", sizeof(ARR_TYPE)*(BS*BS));
+    printf("Kernel size:                %dx%d\n", MR, NR);
     printf("%s\n", HLINE);
     printf("Elements of array A: %d\n", M*K); 
     printf("Elements of array B: %d\n", K*N); 
@@ -158,7 +222,10 @@ void summary(void) {
 }
 
 int main(void) {
-    assert(N%BS==0 && M%BS==0 && K%BS==0);
+#ifndef __AVX2__
+    fprintf(stderr, "AVX2 not supported.");
+    exit(EXIT_FAILURE);
+#endif
     /* the pointer returned from malloc is suitably alligned
      * https://pubs.opengroup.org/onlinepubs/7908799/xsh/malloc.html */
     ARR_TYPE *A = (ARR_TYPE*)malloc(sizeof(ARR_TYPE) * M*K);
@@ -208,12 +275,15 @@ int main(void) {
         gflops = (flops/time)*1e-9;
         printf("%.*f\t\t%.*fms\n", precision, gflops, precision, time*1e3);
     }
-    printf("%s\n", HLINE);
 
+    printf("%s\n", HLINE);
     printf("Best time:          %.*fms\n", precision, best_time*1e3);
     printf("Best time GFLOPS:   %.*f\n", precision, (flops/best_time)*1e-9);
     printf("%s\n", HLINE);
 
+    /* Otherwise clang with -O3 will assume that C is "dead" and delete all the FMA istructions */
+    volatile ARR_TYPE sink = C[0];
+    (void)sink;
     free(A); free(B); free(C);
     exit(EXIT_SUCCESS);
 }
