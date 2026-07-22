bd3aba0a12032bd233b8e66e8df3297c2ae6072d
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-22 16:59:38 +0200

    2D matmul test

--- bench/bench.c
+++ bench/bench.c
@@ -6,13 +6,13 @@
 int main(void) {
     srand(time(NULL));
     
-    cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 2, ((uint16_t[]){3, 1}), 3.f);
-    cfm_tensor *B = cfm_tensor_full("B", CFM_FLOAT32, 1, ((uint16_t[]){3}), 3.f);
+    cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
+    cfm_tensor *B = cfm_tensor_full("B", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
 
     cfm_tensor_print(A, 2);
     cfm_tensor_print(B, 2);
 
-    cfm_tensor *C = cfm_tensor_dot("C", A, B);
+    cfm_tensor *C = cfm_tensor_matmul("C", A, B);
     cfm_tensor_print(C, 2);
 
     cfm_tensor_free(A);
