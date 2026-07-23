c6b73d22243d2912af3152822ef500105c136230
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-23 13:02:26 +0200

    u 1D and v 2D test

--- bench/bench.c
+++ bench/bench.c
@@ -6,7 +6,7 @@
 int main(void) {
     srand(time(NULL));
     
-    cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
+    cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 1, ((uint16_t[]){3}), 3.f);
     cfm_tensor *B = cfm_tensor_full("B", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
 
     cfm_tensor_print(A, 2);
