79ff0c4f2900ba3eb6663e67c154c9fa2e514942
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-23 18:31:00 +0200

    cfm_set/get_num_threads test

--- bench/bench.c
+++ bench/bench.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 #include "../cfm.h"
 
 int main(void) {
+    cfm_set_num_threads(4);
+#ifdef _OPENMP
+    printf("Running on %d threads.\n", cfm_get_num_threads());
+#endif
     srand(time(NULL));
     
     cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
-    cfm_tensor *B = cfm_tensor_full("B", CFM_FLOAT32, 1, ((uint16_t[]){3}), 3.f);
+    cfm_tensor *B = cfm_tensor_full("B", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
 
     cfm_tensor_print(A, 2);
     cfm_tensor_print(B, 2);
