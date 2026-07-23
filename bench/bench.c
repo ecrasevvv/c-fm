94140bfc3ea2af72e85bc7558d43b1d7e8bdd138
ecrasevvv <francesco.ostuni001@studenti.uniparthenope.it>
2026-07-23 18:53:35 +0200

    cfm_num_threads limit test

--- bench/bench.c
+++ bench/bench.c
@@ -7,10 +7,8 @@
 #include "../cfm.h"
 
 int main(void) {
-    cfm_set_num_threads(4);
-#ifdef _OPENMP
-    printf("Running on %d threads.\n", cfm_get_num_threads());
-#endif
+    cfm_set_num_threads(69);
+    printf("NTHREADS: %d\n", cfm_get_num_threads());
     srand(time(NULL));
     
     cfm_tensor *A = cfm_tensor_full("A", CFM_FLOAT32, 2, ((uint16_t[]){3, 3}), 3.f);
